5dfb72256930927a50c47ce3371c6acdc6f11eac
廖政 <813144314@qq.com>
2025-12-26 17:17:24 +0800

    feat: use common mapServer

--- main.js
+++ main.js
@@ -1,55 +1,24 @@
-import axios from 'axios'
 import './style.css'
 import { Map, View } from 'ol'
 import TileLayer from 'ol/layer/Tile'
-import { XYZ } from 'ol/source'
-import { TileGrid } from 'ol/tilegrid'
-import {defaults as defaultInteractions} from 'ol/interaction';
+import { defaults as defaultInteractions } from 'ol/interaction'
+import ImageTile from 'ol/source/ImageTile'
 
 const mapInst = new Map({
   target: 'map',
   layers: [],
   view: new View({
-    center: [0, 0],
-    zoom: 17,
+    center: [-472202, 7530279],
+    zoom: 12,
   }),
   interactions: defaultInteractions({
     doubleClickZoom: false,
   }),
 })
 
-async function getArcgisParam(url) {
-  const paramUrl = url + `?f=json`
-  const response = await axios.get(paramUrl).then((res) => res)
-  const data = response.data
-  const tileInfo = data.tileInfo
-  if (!tileInfo || response.status != 200) {
-    return
-  }
-  const fullExtent = data.fullExtent
-  const resolutions = tileInfo.lods.map((item) => {
-    return item.resolution
-  })
-  const extent = [fullExtent.xmin, fullExtent.ymin, fullExtent.xmax, fullExtent.ymax]
-  return {
-    url: url + '/tile/{z}/{y}/{x}',
-    extent: extent,
-    resolutions: resolutions,
-    tileInfo: tileInfo,
-  }
-}
-
 async function addTileLayer(layerInfo) {
-  const param = await getArcgisParam(layerInfo.url)
-  const source = new XYZ({
-    crossOrigin: 'anonymous',
-    url: param.url,
-    tileGrid: new TileGrid({
-      tileSize: param.tileInfo.rows,
-      origin: [param.tileInfo.origin.x, param.tileInfo.origin.y],
-      resolutions: param.resolutions,
-      extent: param.extent,
-    }),
+  const source = new ImageTile({
+    url: layerInfo.url,
   })
   const layer = new TileLayer({
     type: layerInfo.type,
@@ -57,36 +26,48 @@ async function addTileLayer(layerInfo) {
     zIndex: layerInfo.zIndex,
   })
   mapInst.addLayer(layer)
-  mapInst.getView().fit(param.extent, { duration: 500 })
 }
 
 export const testData = {
-  newImgUrl: '',  // your url
-  oldImgUrl: '',  // your url
+  newImgUrl:
+    'https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=0e6fc415256d4fbb9b5166a718591d71',
+  oldImgUrl:
+    'https://{a-c}.tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=0e6fc415256d4fbb9b5166a718591d71',
 }
 
 addTileLayer({ url: testData.newImgUrl, type: 'new', zIndex: 10 })
 addTileLayer({ url: testData.oldImgUrl, type: 'old', zIndex: 9 })
 
+function checkMissParam() {
+  const urlParams = new URLSearchParams(window.location.search)
+  return urlParams.get('miss') === '1'
+}
+
 function downHandler(evt) {
-  mapInst
-    .getLayers()
-    .getArray()
-    .forEach((lay) => {
-      const t = lay.get('type')
-      if (t === 'old') lay.setVisible(true)
-      if (t === 'new') lay.setVisible(false)
-    })
+  console.log('down')
+  if (checkMissParam()) {
+    mapInst
+      .getLayers()
+      .getArray()
+      .forEach((lay) => {
+        const t = lay.get('type')
+        if (t === 'old') lay.setVisible(true)
+        if (t === 'new') lay.setVisible(false)
+      })
+  }
 }
 function upHandler(evt) {
-  mapInst
-    .getLayers()
-    .getArray()
-    .forEach((lay) => {
-      const t = lay.get('type')
-      if (t === 'old') lay.setVisible(false)
-      if (t === 'new') lay.setVisible(true)
-    })
+  console.log('up')
+  if (checkMissParam()) {
+    mapInst
+      .getLayers()
+      .getArray()
+      .forEach((lay) => {
+        const t = lay.get('type')
+        if (t === 'old') lay.setVisible(false)
+        if (t === 'new') lay.setVisible(true)
+      })
+  }
 }
 mapInst.getTargetElement().addEventListener('pointerdown', downHandler)
 mapInst.getTargetElement().addEventListener('pointerup', upHandler)
--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
-My URL only works on a LAN, so you need to set the URL in main.js.
-```javascript
-export const testData = {
-  newImgUrl: '',  // your url
-  oldImgUrl: '',  // your url
-}
-```
+Reference to this example: https://openlayers.org/en/latest/examples/xyz.html
+I set up pointerDown and pointerUp events to detect double clicks.
+
+In the URL http://localhost:5173/?miss=1, you can see that the double-click event is not triggered.
+In the URL http://localhost:5173/, you can see that the double-click event is triggered.
+This issue also occurs in other examples, such as https://openlayers.org/en/v9.2.4/examples/xyz.html.
+
+Finally, I promise that I will not use that API key for any other purposes.
